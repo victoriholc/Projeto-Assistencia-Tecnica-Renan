5c921eb249426dc4ac5b112701155be6f9d3e09e
Raphael Santos <Raphael Santos@10.1.7.4>
2013-11-26 01:16:09 -0200

    Precisa Fazer o direcionamento entre as faces do produto, preciso de ajuda

--- src/br/com/assistenciaTecnica/cadastral/managedBeans/ProductBean.java
+++ src/br/com/assistenciaTecnica/cadastral/managedBeans/ProductBean.java
@@ -17,15 +17,26 @@ public class ProductBean {
 	private Product product;
 	private String searchField = "";
 	private Collection<Product> listProduct = new ArrayList<Product>();
+	private String selectedObject;
 	
-	
-	public ProductBean(Product product, String searchField, Collection<Product> listProduct) {
+	public ProductBean(Product product, String searchField, Collection<Product> listProduct,String selectedObject) {
 		super();
 		this.product = new Product();
 		this.searchField = searchField;
 		this.listProduct = new ArrayList<Product>();
+		this.selectedObject = selectedObject;
 	}
 	
+	
+	public String getSelectedObject() {
+		return selectedObject;
+	}
+
+	public void setSelectedObject(String selectedObject) {
+		this.selectedObject = selectedObject;
+	}
+
+
 	public String getSearchField() {
 		return searchField;
 	}
@@ -50,7 +61,7 @@ public class ProductBean {
 		this.product = product;
 	}
 	
-	public void insertProduct(){
+	public String insertProduct(){
 		try{
 			if(product.getId() == 0){
 				Facade.getInstace().insertProduct(this.product);
@@ -58,22 +69,30 @@ public class ProductBean {
 				Facade.getInstace().updateProduct(this.product);
 			}
 		}catch(ProductAlreadyExistsException e){
-			FacesContext.getCurrentInstance().addMessage(null , new FacesMessage("Produto j� existe" + 
+			FacesContext.getCurrentInstance().addMessage(null , new FacesMessage("Produto ja existe" + 
 			this.product.getNumberSerial()));
 			e.getMessage();
-			System.out.println("Produto j� existe");
-		}		
+			System.out.println("Produto ja existe");
+		}	
+		return "/cadastral/produto/view_product.xhtml";
 	}
 	
-	public void removeProduct(Product product){
+	public String removeProduct(Product product){
 		try{ 
 			Facade.getInstace().removeProduct(product);
 		}catch(Exception e){
-			FacesContext.getCurrentInstance().addMessage(null,new FacesMessage("Produto n�o pode ser removido" +
+			FacesContext.getCurrentInstance().addMessage(null,new FacesMessage("Produto nao pode ser removido" +
 			product.getModel()));
 			
-			System.out.println("Produto n�o pode ser removido");
+			System.out.println("Produto nao pode ser removido");
 		}
+		return "/cadastral/produto/view_product.xhtml";
+	}
+	
+	public String updateProduct(Product param){
+		this.product = param;
+		this.selectedObject = param.getMark();
+		return "/cadastral/produto/save_product.xhtml";
 	}
 	
 	public void seeAllProduct(){
@@ -85,4 +104,9 @@ public class ProductBean {
 			e.getMessage();
 		}	
 	}
+	
+	public String viewProduct() {//metodo de redirecionamento para voltar pagina
+		return "/cadastral/product/view_product.xhtml?faces-redirect=true";
+	}
+
 }
